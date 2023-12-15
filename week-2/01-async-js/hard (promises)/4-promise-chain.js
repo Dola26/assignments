84aff722011e47e17f035e6aaf32d48f6ff1dd8c
Dola26 <dolapavan@gmail.com>
2023-12-15 22:53:09 +0530

    week -2 tests were updated for async soln

--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -6,18 +6,50 @@
  */
 
 function wait1(t) {
+    return new Promise ((resolve)=>{
+        setTimeout(()=>{
+            resolve();
+        },t*1000)
+
+    })
 
 }
 
 function wait2(t) {
+    return new Promise ((resolve)=>{
+        setTimeout(()=>{
+            resolve();
+        },t*1000)
+
+    })
+
 
 }
 
 function wait3(t) {
+    return new Promise ((resolve)=>{
+        setTimeout(()=>{
+            resolve();
+        },t*1000)
+
+    })
+
 
 }
-
 function calculateTime(t1, t2, t3) {
+    const time1=Date.now();
+
+    return wait1(t1).then(()=>{
+        return wait2(t2)
+    }).then(()=>{
+        return wait3(t3)
+    }).then(()=>{
+        const time3=Date.now();
+        const total=time3-time1;
+        return total;
+    })
+
+
 
 }
 

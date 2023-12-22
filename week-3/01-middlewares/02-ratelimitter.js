4b9c3c99f515cd3453174cbb44e4c7826b0578c0
Dola26 <dolapavan@gmail.com>
2023-12-22 18:04:18 +0530

    week -3 file middleware assign cmplted

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -11,10 +11,27 @@ const app = express();
 // You have been given a numberOfRequestsForUser object to start off with which
 // clears every one second
 
+
 let numberOfRequestsForUser = {};
 setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
+
+app.use((req,res,next)=>{
+  const userid=req.headers['user-id']
+  if(numberOfRequestsForUser[userid]){
+    numberOfRequestsForUser[userid]++;
+    if(numberOfRequestsForUser[userid]>5){
+      res.status(404).send("user banned");
+    }else{
+      next();
+    }
+  }else{
+    numberOfRequestsForUser[userid]=1;
+    next();
+  }
+
+})
 
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
@@ -23,5 +40,7 @@ app.get('/user', function(req, res) {
 app.post('/user', function(req, res) {
   res.status(200).json({ msg: 'created dummy user' });
 });
+
+clearInterval(numberOfRequestsForUser);
 
 module.exports = app;
